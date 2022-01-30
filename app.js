41721f96f448a5e3a3e8b5c9de5cb8fb3eddc546
ujangaja <ujangaja@gmail.com>
2022-01-30 17:29:42 +0700

    324. Lest take the TodoList project to the Next Level and Connect it with Mongoose

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -11,13 +11,41 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const items = ['Buy Food', 'Cook Food', 'Eat Food'];
-const workItems = [];
+// connect to database
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
+  useNewUrlParser: true,
+});
+
+// Schema database
+const itemsSchema = {
+  name: String,
+};
+
+const Item = new mongoose.model('Item', itemsSchema);
+
+// insert document
+const item1 = new Item({
+  name: 'welcome to your todolist!',
+});
+const item2 = new Item({
+  name: 'Hit the + buttom to aff a new item.',
+});
+const item3 = new Item({
+  name: '<-- Hit this to delete an item.',
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Succesfully saved default items to DB.');
+  }
+});
 
 app.get('/', function (req, res) {
-  const day = date.getDate();
-
-  res.render('list', { listTitle: day, newListItems: items });
+  res.render('list', { listTitle: 'Today', newListItems: items });
 });
 
 app.post('/', function (req, res) {
--- readme.md
+++ readme.md
@@ -3,3 +3,20 @@
     Todolist-v2-starting-files
 
     npm i
+
+## 324. Lest take the TodoList project to the Next Level and Connect it with Mongoose
+
+    npm i mongoose
+
+    restart sever nodemon app.js
+
+    pada terminal baru , masuk kedalam mongodb:
+    mongo
+    show dbs
+    masuk kedalam todolistDB: use todolistDB
+    > show collections
+    items
+    > db.items.find()
+    { "_id" : ObjectId("61f667ac1667d9ff268f493b"), "name" : "welcome to your todolist!", "__v" : 0 }
+    { "_id" : ObjectId("61f667ac1667d9ff268f493c"), "name" : "Hit the + buttom to aff a new item.", "__v" : 0 }
+    { "_id" : ObjectId("61f667ac1667d9ff268f493d"), "name" : "<-- Hit this to delete an item.", "__v" : 0 }
